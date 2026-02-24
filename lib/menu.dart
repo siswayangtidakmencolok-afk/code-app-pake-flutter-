a4a1309e7dda6d290ee2165f1954c2a50d9e33f8
FHAZWAN ATHAR RAMADHAN <siswayangtidakmencolok@gmail.com>
2026-02-24 21:27:46 +0700

    Add files via upload

---
+++ lib/menu.dart
@@ -0,0 +1,32 @@
+// menu.dart
+final List<Map<String, dynamic>> menu = [
+  {
+    "nama": "Burger",
+    "harga": 25000,
+    "gambar":
+        "https://static.vecteezy.com/system/resources/previews/022/911/697/non_2x/burger-transparent-free-png.png",
+  },
+  {
+    "nama": "Nasi Goreng",
+    "harga": 20000,
+    "gambar":
+        "https://png.pngtree.com/png-clipart/20220509/original/pngtree-makanan-indonesia-nasi-goreng-png-image_7690129.png",
+  },
+  {
+    "nama": "Ayam Goreng",
+    "harga": 30000,
+    "gambar":
+        "https://png.pngtree.com/png-clipart/20230421/original/pngtree-menu-for-fried-chicken-with-spicy-sambal-png-image_9070988.png",
+  },
+  {
+    "nama": "Es Teh",
+    "harga": 8000,
+    "gambar":
+        "https://w7.pngwing.com/pngs/908/384/png-transparent-green-tea-ice-tea.png",
+  },
+  {
+    "nama": "Jus Jeruk",
+    "harga": 12000,
+    "gambar": "https://cdn-icons-png.flaticon.com/512/415/415734.png",
+  },
+];
